854f4a4f623a361ba5d89aa8087d0bfd33b4a5d1
PhilippeWS <91435899+PhilippeWS@users.noreply.github.com>
2022-04-16 17:21:08 -0700

    Created two functions, one to retrieve the data from the raw source on github, the other to create and store it in a local directory.

---
+++ WordRetriever.py
@@ -0,0 +1,20 @@
+import urllib.request
+import os
+
+raw_data_source = "https://raw.githubusercontent.com/tabatkins/wordle-list/main/words"
+directory_name = "WordleData";
+file_name = "wordList.txt"
+
+def retrieve_data():
+    url_request = urllib.request.urlopen(raw_data_source);
+    data = url_request.read().decode("utf-8")
+    return data
+
+def store_data():
+    data = retrieve_data()
+    os.mkdir(directory_name)
+    file = open(directory_name + "/" + file_name, "w")
+    file.write(data)
+    file.close
+
+store_data()
